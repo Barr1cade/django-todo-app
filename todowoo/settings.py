089ca140b3967f1fe1695b86802d70dc91040d50
Nicolas <kolyakotik.vbg@gmail.com>
2021-07-16 17:58:54 +0300

    debug mode False

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(1+-e^r(&p2vco8iykiy+)mm#!(=(xklkc)(xfid5a33tm80ax'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
